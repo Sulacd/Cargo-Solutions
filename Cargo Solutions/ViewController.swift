11164e70176aa04f3712de92a9531c060a2b9f30
Christian Diaz <sulacidi@gmail.com>
2022-11-17 14:59:31 -0800

    Initial Commit

---
+++ Cargo Solutions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cargo Solutions
+//
+//  Created by Christian Diaz on 11/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
